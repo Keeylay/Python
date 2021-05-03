e5829e315bb82fd39c5359e22e0ca8c65fae5432
Keeylay <kyrutkowski@gmail.com>
2021-05-03 15:37:43 -0500

    changes

--- fundamentals/introduction/RockPaperScissors.py
+++ fundamentals/introduction/RockPaperScissors.py
@@ -1,28 +1,37 @@
 import random
 
-action = input("Enter one of the following: Rock, Paper, Scissors:")
+while True:
+    manygames = int(input("Enter a number for how many games do you want to play "))
+    for i in range(manygames):
+        if (manygames > 0):
+            manygames = manygames - 1
 
-possibilities = ["Rock", "Paper", "Scissors"]
+            action = input("Enter one of the following: Rock, Paper, Scissors:")
+            possibilities = ["Rock", "Paper", "Scissors"]
+            computerchoice = random.choice(possibilities)
 
-computerchoice = random.choice(possibilities)
-
-#win conditions
-if action == computerchoice:
-    print("Its a tie!")
-elif action == "Rock":
-    if computerchoice == "Scissors":
-        print("Rock smashes Scissors! You win!")
-    else:
-        print("Rock cant win against Paper! You Lose!")
-elif action == "Paper":
-    if computerchoice == "Scissors":
-        print("Scissors slices your Paper! You Lose!")
-    else: 
-        print("Rock cant win against Paper! You Win!")
-elif action == "Scissors":
-    if computerchoice == "Paper":
-        print("Scissors slices the Paper! You Win!")
-    else:
-        print("Rock smashes Scissors! You Lose!")
-
-print(f"You choose {action} and the computer choose {computerchoice}.")
+            #win conditions
+            if action == computerchoice:
+                print("Its a tie!")
+            elif action == "Rock":
+                if computerchoice == "Scissors":
+                    print("Rock smashes Scissors! You win!")
+                else:
+                    print("Rock cant win against Paper! You Lose!")
+            elif action == "Paper":
+                if computerchoice == "Scissors":
+                    print("Scissors slices your Paper! You Lose!")
+                else: 
+                    print("Rock cant win against Paper! You Win!")
+            elif action == "Scissors":
+                if computerchoice == "Paper":
+                    print("Scissors slices the Paper! You Win!")
+                else:
+                    print("Rock smashes Scissors! You Lose!")
+        
+            print(f"You choose {action} and the computer choose {computerchoice}.")
+            
+            if (manygames != 0):
+                continue
+            else:
+                quit()
